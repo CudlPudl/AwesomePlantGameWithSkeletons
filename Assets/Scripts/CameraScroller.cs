a51a2571038c72a494f22b66858ef6ed16454f85
Cudl <tossubot@gmail.com>
2020-09-12 11:25:10 +0300

    Camera scroller, cinemachine.

---
+++ Assets/Scripts/CameraScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraScroller : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineDollyCart _cart;
+    [SerializeField]
+    private Camera _camera;
+    private Vector3 _touchStart;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _touchStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = _touchStart - Input.mousePosition;
+            direction /= Screen.currentResolution.width;
+            _cart.m_Position += direction.x;
+        }
+    }
+}
